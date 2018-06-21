035619af8538ede5250b4762065d0582cdff9299
Bruno Pimentel <bpimentel@mastertech.tech>
2018-06-21 10:06:06 -0300

    Finalizar exemplos

---
+++ traduzir.js
@@ -0,0 +1,13 @@
+const dicionario = require('./dicionario.json');
+
+let palavra = process.argv[2];
+let traducao = dicionario[palavra];
+
+if(palavra == 'listar-tudo'){
+    console.log(dicionario);
+}else if(traducao == undefined){
+    console.log('Não tem essa palavra, pô!');
+}else{
+    console.log(traducao);
+}
+
